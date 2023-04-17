5f52b3870edc6b05d308ca857f0bee1be0fde2a6
Abram Siemsen <abram@siemsen.us>
2023-04-17 17:15:59 -0500

    Add deliverable and cycle classes demo

---
+++ projects/game-ui/cycle-classes-demo/js/scripts.js
@@ -0,0 +1,28 @@
+// Make sure the document is loaded and ready to go.
+document
+    .addEventListener('DOMContentLoaded', () => {
+
+        // document
+        //     .getElementById('status-bar-1')
+        //     .addEventListener('click', () => {
+        //         document
+        //             .getElementById('status-bar-1')
+        //             .classList
+        //             .toggle('high')
+        //     })
+
+        // document
+        //     .getElementById('cycle-status-states-btn')
+        //     .addEventListener('click', () => {
+        //         const statusBar = document
+        //             .getElementById('status-bar-5')
+        //         const cycleClasses = statusBar.dataset.cycleclasses.split(',')
+        //         const currentClass = statusBar.dataset.currentclass
+        //         const position = cycleClasses.indexOf(currentClass)
+        //         const nextClass = cycleClasses[position + 1] || cycleClasses[0]
+        //         statusBar.dataset.currentclass = nextClass
+        //         statusBar.classList.remove(currentClass)
+        //         statusBar.classList.add(nextClass)
+        //     })
+
+    })
